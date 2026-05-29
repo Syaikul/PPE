f5c918a1076927a50ac3e9409d5a37bfa6310e5b
user <syaikulibnu0@gmail.com>
2026-05-29 15:36:21 +0700

    upload pertama

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,217 @@
+<div class="sidebar" style="background-color: #e8f0ec !important; color: white;">
+    <div class="sidebar-logo">
+
+        <!-- Logo Header -->
+        <div class="logo-header" style="background-color: #e8f0ec !important; color: white;">
+            <a href="index.html" class="logo">
+                <img src="https://www.mesitechmitra.co.id/assets/photos/logo-baru.png" alt="navbar brand"
+                    class="navbar-brand" height="40" />
+            </a>
+
+            <div class="nav-toggle">
+                <button class="btn btn-toggle toggle-sidebar">
+                    <i class="gg-menu-right"></i>
+                </button>
+
+                <button class="btn btn-toggle sidenav-toggler">
+                    <i class="gg-menu-left"></i>
+                </button>
+            </div>
+
+            <button class="topbar-toggler more">
+                <i class="gg-more-vertical-alt"></i>
+            </button>
+        </div>
+        <!-- End Logo Header -->
+
+    </div>
+
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+
+            <ul class="nav nav-secondary">
+
+                <!-- Dashboard -->
+                <li class="nav-item">
+                    <a href="widgets.html">
+                        <i class="fas fa-home"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+
+                <!-- Data Stok -->
+                <li class="nav-item active">
+                    <a data-bs-toggle="collapse" href="#datastok" class="collapsed" aria-expanded="false">
+
+                        <i class="fas fa-desktop"></i>
+                        <p>Data Stok</p>
+                        <span class="caret"></span>
+                    </a>
+
+                    <div class="collapse" id="datastok">
+                        <ul class="nav nav-collapse">
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">PPE Masuk</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">PPE Keluar</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Stok</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Personel -->
+                <li class="nav-item active">
+                    <a data-bs-toggle="collapse" href="#personel" class="collapsed" aria-expanded="false">
+
+                        <i class="fas fa-users"></i>
+                        <p>Personel</p>
+                        <span class="caret"></span>
+                    </a>
+
+                    <div class="collapse" id="personel">
+                        <ul class="nav nav-collapse">
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Data Personel</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Data Pemakaian PPE</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Permintaan PPE -->
+                <li class="nav-item active">
+                    <a data-bs-toggle="collapse" href="#permintaanppe" class="collapsed" aria-expanded="false">
+
+                        <i class="fas fa-clipboard-list"></i>
+                        <p>Permintaan PPE</p>
+                        <span class="caret"></span>
+                    </a>
+
+                    <div class="collapse" id="permintaanppe">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Buat Tabel Permintaan</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Data Permintaan</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Mob-Demob -->
+                <li class="nav-item active">
+                    <a data-bs-toggle="collapse" href="#mobdemob" class="collapsed" aria-expanded="false">
+
+                        <i class="fas fa-truck"></i>
+                        <p>Mob-Demob</p>
+                        <span class="caret"></span>
+                    </a>
+
+                    <div class="collapse" id="mobdemob">
+                        <ul class="nav nav-collapse">
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Approval Demob</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Mobilisasi</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Demobilisasi</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Peminjaman PPE -->
+                <li class="nav-item active">
+                    <a data-bs-toggle="collapse" href="#peminjamanppe" class="collapsed" aria-expanded="false">
+
+                        <i class="fas fa-handshake"></i>
+                        <p>Peminjaman PPE</p>
+                        <span class="caret"></span>
+                    </a>
+
+                    <div class="collapse" id="peminjamanppe">
+                        <ul class="nav nav-collapse">
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Pinjam</span>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="">
+                                    <span class="sub-item">Dipinjam</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </li>
+
+                <!-- Spare Barang -->
+                <li class="nav-item">
+                    <a href="widgets.html">
+                        <i class="fas fa-box-open"></i>
+                        <p>Spare Barang</p>
+                    </a>
+                </li>
+
+                <!-- Filter -->
+                <li class="nav-item">
+                    <a href="widgets.html">
+                        <i class="fas fa-box-open"></i>
+                        <p>Filter</p>
+                    </a>
+                </li>
+
+                <!-- User -->
+                <li class="nav-item">
+                    <a href="widgets.html">
+                        <i class="fas fa-user"></i>
+                        <p>User</p>
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+    </div>
+</div>
